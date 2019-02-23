7a33f39ab7b881408001a80d3d357c4eb17c5ef0
K.R. Zentner <krzentner@gmail.com>
2019-02-22 20:55:44 -0800

    Fix error from importing theano.tensor as TT

--- src/akro/theano/_util.py
+++ src/akro/theano/_util.py
@@ -1,7 +1,7 @@
 """Private module containing utilities for working with Theno."""
-import theano.tensor as TT
+import theano.tensor
 
 
 def new_tensor(name, ndim, dtype):
     """Return a new tensor based on the data type and name provided."""
-    return TT.TensorType(dtype, (False, ) * ndim)(name)
+    return theano.tensor.TensorType(dtype, (False, ) * ndim)(name)
