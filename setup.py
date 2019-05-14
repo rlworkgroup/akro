6f3bc46b2c682d6543ed0c0f7d53bd013e6b41d1
Ryan Julian <ryanjulian@users.noreply.github.com>
2019-05-14 15:47:40 -0700

    Rewrite automatic versioning (#35)
    The previous automatic versioning script was flawed. It produced the
    correct package version for documentation builds and building PyPI
    distributions, but produced an incorrect version when you run setup.py
    from the downloaded package. Unfortunately, Python environment managers
    (e.g. Pipenv, conda) resolve package version by evaluating setup.py,
    not using the PyPI version.
    
    This PR makes version generation simpler by reading the version string
    from a simple file. Automatic versioning from tags is handled by
    clobbering the version file from within the CI, rather than looking for
    a CI environment variable on every usage.

--- setup.py
+++ setup.py
@@ -36,8 +36,8 @@ with open('README.md') as f:
     readme = f.read()
 
 # Get the package version dynamically
-exec(open('./src/akro/__version__.py').read())
-version = __version__  # noqa: F821, pylint: disable=undefined-variable
+with open('VERSION') as v:
+    version = v.read().strip()
 
 setup(
     name='akro',
